f1c28947a9d3f1c547bf31c8de7bca7a24b1399e
Gregory Izuka <gizuka@seamfix.com>
2024-09-26 22:46:09 +0100

    added static data

---
+++ app/components/data.ts
@@ -0,0 +1,61 @@
+import Scrol1 from "../../public/icons/scrol1.svg";
+import Scrol2 from "../../public/icons/scrol2.svg";
+import Scrol3 from "../../public/icons/scrol3.svg";
+import Scrol4 from "../../public/icons/scrol4.svg";
+import Scrol5 from "../../public/icons/scrol5.svg";
+import Pix from "../../public/icons/potrait.svg";
+
+export const itemData = [
+    {
+        title: "평균 월 120만원",
+        description: "임금을 해당 국가를 기준으로 계산합니다."
+    },
+    {
+        title: "최대 3회 인력교체",
+        description: "막상 채용해보니 맞지 않아도 걱정하지 마세요. "
+    },
+    {
+        title: "평균 3일, 최대 10일",
+        description: "급하게 사람이 필요한 경우에도 빠른 채용이 가능합니다. "
+    }
+]
+
+export const scrollingCardData = [
+    {
+        text: "해외 마케팅",
+        icon: Scrol1
+
+    },
+    {
+        text: "퍼블리셔",
+        icon: Scrol2
+
+    },
+    {
+        text: "캐드원(제도사)",
+        icon: Scrol3
+
+    },
+    {
+        text: "해외 세일즈",
+        icon: Scrol4
+
+    },
+    {
+        text: "해외 세일즈",
+        icon: Scrol5
+
+    },
+]
+
+export const toggleCardData = [
+    {
+        pix: Pix,
+        name: "Abhishek Gupta",
+        age: "마케팅 · 2y+",
+        detail1: "마케팅 콘텐츠 제작",
+        detail2: "인스타그램 관리",
+        detail3: "트위터 관리",
+        detail4: "블로그 글 작성",
+    }
+]
